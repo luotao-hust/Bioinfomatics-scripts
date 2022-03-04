a5dfe21988e4ef4276a7d3628eb8414c4d2e83c5
luotao <dadaxqu@outlook.com>
2022-03-04 09:56:36 +0800

    Update 10x_bam_to_individual_bam_memory_cost.py

--- possorted_genome_bam_to_individual_bam/10x_bam_to_individual_bam_memory_cost.py
+++ possorted_genome_bam_to_individual_bam/10x_bam_to_individual_bam_memory_cost.py
@@ -1,30 +1,35 @@
 ### Python 3.6.13
-### Using the Python dictionary type to store tmp file
-
-import os
 import pysam
-import time
-import pandas as pd 
-### Input varibles to set
-# file to split on
-unsplit_file = "/workspace/luot/data/EV_data/data/cellranger_out/100K5M/outs/possorted_genome_bam.bam"
-# where to place output files
-out_dir = "/workspace/luot/data/EV_data/data/QC/100K5M_memory_cost/"
-barcode_list_ev = pd.read_csv("/workspace/luot/EV/script/QC/barcode_list.txt")["x"].tolist()
-barcode_dict = {}
-# read in upsplit file and loop reads by line
-samfile = pysam.AlignmentFile(unsplit_file, "rb")
-for read in samfile.fetch(until_eof=True):
-    try:
-        CB_itr = read.get_tag('CB')
-    except:
-        continue
-    if CB_itr not in barcode_list_ev:
-        continue
-    if CB_itr not in barcode_dict.keys():
-        split_file = pysam.AlignmentFile(out_dir + "{}.bam".format(CB_itr), "wb", template=samfile)
-        barcode_dict[CB_itr] = split_file
-    barcode_dict[CB_itr].write(read)
-for key in barcode_dict.keys():
-    barcode_dict[key].close()
-samfile.close()
+from pathlib import Path
+import sys
+import os
+
+def main(input_bam,barcodes_file,output_dir):
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    if Path(barcodes_file).is_file():
+        with open(barcodes_file, "r") as fh:
+            barcode_list = [l.rstrip() for l in fh.readlines()]
+    else:
+        barcode_list = [barcodes_file]
+    barcode_dict = {}
+    # read in upsplit file and loop reads by line
+    samfile = pysam.AlignmentFile(input_bam, "rb")
+    for read in samfile.fetch(until_eof=True):
+        try:
+            CB_itr = read.get_tag('CB')
+        except:
+            continue
+        if CB_itr not in barcode_list:
+            continue
+        if CB_itr not in barcode_dict.keys():
+            split_file = pysam.AlignmentFile(os.path.join(output_dir + "/{}.bam".format(CB_itr)), "wb", template=samfile,threads=8)
+            barcode_dict[CB_itr] = split_file
+        barcode_dict[CB_itr].write(read)
+    samfile.close()
+    for key in barcode_dict.keys():
+        barcode_dict[key].close()
+        
+if __name__ == "__main__":
+    main(input_bam=sys.argv[1],barcodes_file=sys.argv[2],output_dir=sys.argv[3])
+# python split_bc_bam_memory_cost.py /workspace/luot/data/EV_data/data/cellranger_out/100K5M/outs/possorted_genome_bam.bam /workspace/luot/EV/QC/mapping_rates/K562_100_EV_barcode_list.txt  /workspace/luot/EV/QC/mapping_rates/K562_100_EV
