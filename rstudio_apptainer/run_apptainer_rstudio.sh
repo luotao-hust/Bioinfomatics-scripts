2f5a576dd59f91f539a2086639494f61b6c62a94
luotao <taoluo@hust.edu.cn>
2023-03-23 15:06:40 +0800

    Update run_apptainer_rstudio.sh

--- rstudio_apptainer/run_apptainer_rstudio.sh
+++ rstudio_apptainer/run_apptainer_rstudio.sh
@@ -105,7 +105,7 @@ RSTUDIO_SIF=${basedir}/apptainer_jupyterlab_rstudio-server/
 # RSTUDIO_SIF="/***/rstudio_latest.sif" # 镜像的地址,可自己修改
 
 if [ -d "${RSTUDIO_SIF}" ];then
-    ;;
+    :
     else        
         echo -e "\033[34mINFO:\033[0m Extracting files.........."
         apptainer build --sandbox ${RSTUDIO_SIF} ${OR_RSTUDIO_SIF}
