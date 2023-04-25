6492848399ed90b46ee7edfff0b921077683a141
luotao <taoluo@hust.edu.cn>
2023-04-25 15:54:04 +0800

    Update run_apptainer_rnaseq.sh

--- run_apptainer_rnaseq/run_apptainer_rnaseq.sh
+++ run_apptainer_rnaseq/run_apptainer_rnaseq.sh
@@ -1,1 +1,44 @@
+#!/bin/bash
 
+while getopts ":o:s:a:g:x:c:b:t:f:d:m:" arg;
+    do
+        case $arg in
+             o)
+                OUTPUT_DIR=$OPTARG
+                ;;
+             s)
+                SAMPLE_LIST=$OPTARG
+                ;;
+             a)
+                SIF=$OPTARG
+                ;;
+             g)
+                GTF=$OPTARG
+                ;;
+             x)
+                HISAT2_INDEX=$OPTARG
+                ;;
+             c)
+                CORES=$OPTARG
+                ;;
+             b)
+                EXBIND=$OPTARG
+        ;;
+             t)
+                TYPE=$OPTARG
+        ;;   
+             f)
+                FASTP=$OPTARG
+                ;;
+             d)
+                DRY=$OPTARG
+                ;;
+         m)
+            MODEL=$OPTARG
+        ;;
+             ?)
+                echo "Invalid option: -$OPTARG"
+                exit 1
+                ;;
+        esac
+    done
